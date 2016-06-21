0e22017ee37e4596b94b3850072b8a2a84d59e5a
Teddy Limousin <mongaru007@gmail.com>
2016-06-21 08:20:50 -0400

    Se agregan los primeros archivos del proyecto

---
+++ pti_pesepy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pti_pesepy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pti_pesepy.settings")
+
+application = get_wsgi_application()
